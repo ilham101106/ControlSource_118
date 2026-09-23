8bb6f3f763d776bec47e8e353a4a88eda1b9b4a8
ilham101106 <pesatoffical321@gmail.com>
2026-09-23 14:14:00 +0700

    membuat gambar dengan looping turtle

--- deposit.py
+++ deposit.py
@@ -1,5 +1,5 @@
 
 deposit = 150
 if deposit > 100 :
-    print ("ugadgoqa") 
+    print ("you got a free toaster!") 
 print("hace a nice day")
---
+++ if,then,otherwise.py
@@ -0,0 +1,10 @@
+country = input ("where are you from ? : ")
+
+if country == "India" :
+    print ("prindapan lu")
+elif country == "Indonesia " :
+    print ("negara korup")
+elif country == "China" :
+    print ("Hallo")
+else :
+    print ("ya")
